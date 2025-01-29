988eacd4f44a8e58af64be989dd7955940498b90
Vagnerbs <vagnerb1987@gmail.com>
2025-01-29 13:59:56 -0300

    Adicionando dependência no Pom.xml e acessando um arquivo .csv

---
+++ src/main/java/br/com/alura/Main.java
@@ -0,0 +1,21 @@
+package br.com.alura;
+
+import br.com.alura.model.Produto;
+import com.opencsv.bean.CsvToBeanBuilder;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.List;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) throws FileNotFoundException {
+        List<Produto> produtos = new CsvToBeanBuilder(new FileReader("src/main/resources/products.csv"))
+                .withType(Produto.class).build().parse();
+
+        for (Produto produto: produtos){
+            System.out.println(produto);
+        }
+    }
+}
